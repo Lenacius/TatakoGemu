046a116ed59d0defbb97c5084b9cfb38daf37341
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-09-29 01:17:13 -0300

    Base Title Created
    Created Title Scene
    Added new font "A Abstract Groovy"
    Created SceneController script with method GoToLobby()

--- Assets/Scripts/RpcTest.cs
+++ Assets/Scripts/RpcTest.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class RpcTest : MonoBehaviour
+public class RpcTest : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnNetworkSpawn()
     {
-        
+        if (IsClient)
+        {
+            TestServerRpc(0);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [ClientRpc]
+    void TestClientRpc(int value)
     {
-        
+        if (IsClient)
+        {
+            Debug.Log("Client Received the RPC #" + value);
+            TestServerRpc(value + 1);
+        }
+    }
+
+    [ServerRpc]
+    void TestServerRpc(int value)
+    {
+        Debug.Log("Server Received the RPC #" + value);
+        TestClientRpc(value);
     }
 }

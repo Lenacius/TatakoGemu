f6455f5fb12a109f2626d6efb69dc120f0c8cc75
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-09-29 03:56:52 -0300

    Added behaviors for Client and Host
    Both Host and Client now call their respectives Start methods;
    Added ApprovalCheck callback for when a client connects to server;
    Added Handle methods for ServerStarted, ClientConnected, ClientDisconect events;

--- Assets/Scripts/PasswordNetworkController.cs
+++ Assets/Scripts/PasswordNetworkController.cs
@@ -1,6 +1,7 @@
 using Unity.Netcode;
 using UnityEngine;
 using TMPro;
+using System.Text;
 
 public class PasswordNetworkController : MonoBehaviour
 {
@@ -8,9 +9,106 @@ public class PasswordNetworkController : MonoBehaviour
     [SerializeField] private GameObject passwordEntryUI;
     [SerializeField] private GameObject leaveButton;
 
-    public void Host() { }
+    private void Start()
+    {
+        NetworkManager.Singleton.OnServerStarted += HandleServerStarted;
+        NetworkManager.Singleton.OnClientConnectedCallback += HandleClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += HandleClientDisconnect;
+    }
 
-    public void Client() { }
+    private void OnDestroy()
+    {
+        if (NetworkManager.Singleton == null) return;
 
-    public void Leave() { }
+        NetworkManager.Singleton.OnServerStarted -= HandleServerStarted;
+        NetworkManager.Singleton.OnClientConnectedCallback -= HandleClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= HandleClientDisconnect;
+    }
+
+    public void Host() { // host does not count in the connected client count
+        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
+        NetworkManager.Singleton.StartHost();
+        //Debug.Log("Numero de jogadores: " + NetworkManager.Singleton.ConnectedClients.Count);
+    }
+
+    public void Client() {
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = Encoding.ASCII.GetBytes(passwordInputField.text);
+        NetworkManager.Singleton.StartClient();
+        //Debug.Log("Numero de jogadores: " + NetworkManager.Singleton.ConnectedClients.Count);
+    }
+
+    public void Leave() {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            NetworkManager.Singleton.Shutdown();
+            NetworkManager.Singleton.ConnectionApprovalCallback -= ApprovalCheck;
+        }
+        else if (NetworkManager.Singleton.IsClient)
+        {
+            NetworkManager.Singleton.Shutdown();
+        }
+
+        passwordEntryUI.SetActive(true);
+        leaveButton.SetActive(false);
+    }
+
+    private void HandleServerStarted()
+    {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            HandleClientConnected(NetworkManager.Singleton.LocalClientId);
+        }
+    }
+
+    private void HandleClientConnected(ulong clientId)
+    {
+        if(clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            passwordEntryUI.SetActive(false);
+            leaveButton.SetActive(true);
+        }
+    }
+
+    private void HandleClientDisconnect(ulong clientId)
+    {
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            passwordEntryUI.SetActive(true);
+            leaveButton.SetActive(false);
+        }
+    }
+
+    private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
+    {
+        var clientId = request.ClientNetworkId;
+        var connectionData = request.Payload;
+
+        bool approveConnection = Encoding.ASCII.GetString(connectionData) == passwordInputField.text;
+
+        Vector3 spawnPos = Vector3.zero;
+        Quaternion spawnRot = Quaternion.identity;
+
+        switch (NetworkManager.Singleton.ConnectedClients.Count) // comment switch when official
+        {
+            case 1:
+                spawnPos = new Vector3(0f, 0f, 0f);
+                spawnRot = Quaternion.Euler(0f, 180f, 0f);
+                break;
+            case 2:
+                spawnPos = new Vector3(2f, 0f, 0f);
+                spawnRot = Quaternion.Euler(0f, 225f, 0f);
+                break;
+            case 3:
+                spawnPos = new Vector3(-2f, 0f, 0f);
+                spawnRot = Quaternion.Euler(0f, 135f, 0f);
+                break;
+        }
+
+        response.Approved = approveConnection;
+        response.CreatePlayerObject = true;
+        response.PlayerPrefabHash = null;
+        response.Position = spawnPos;
+        response.Rotation = spawnRot;
+        response.Pending = false;
+    }
 }

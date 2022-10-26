4a73562f01c9c76793c4054804a03bdc8195bcf9
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-10-26 00:42:49 -0300

    Tried to implement a payload logic, but failed, commented to do later

--- Assets/Scripts/PasswordNetworkController.cs
+++ Assets/Scripts/PasswordNetworkController.cs
@@ -2,13 +2,17 @@ using Unity.Netcode;
 using UnityEngine;
 using TMPro;
 using System.Text;
+using System.Collections.Generic;
 
 public class PasswordNetworkController : MonoBehaviour
 {
     [SerializeField] private TMP_InputField passwordInputField;
+    [SerializeField] private TMP_InputField nameInputField;
     [SerializeField] private GameObject passwordEntryUI;
     [SerializeField] private GameObject leaveButton;
     [SerializeField] private GameObject teamPicker;
+
+    //private static Dictionary<ulong, string> roomData;
 
     private void Start()
     {
@@ -27,13 +31,27 @@ public class PasswordNetworkController : MonoBehaviour
     }
 
     public void Host() { // host does not count in the connected client count
+        //roomData = new Dictionary<ulong, string>();
+        //roomData[NetworkManager.Singleton.LocalClientId] = passwordInputField.text;
+
         NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
         NetworkManager.Singleton.StartHost();
         //Debug.Log("Numero de jogadores: " + NetworkManager.Singleton.ConnectedClients.Count);
     }
 
+    struct Player
+    {
+        public string name;
+        public string password;
+    }
     public void Client() {
+        //Player player = new Player();
+        //player.name = nameInputField.text;
+        //player.password = passwordInputField.text;
+        //byte[] data = Encoding.ASCII.GetBytes(JsonUtility.ToJson(player));
+        //Debug.Log(JsonUtility.ToJson(player));
         NetworkManager.Singleton.NetworkConfig.ConnectionData = Encoding.ASCII.GetBytes(passwordInputField.text);
+        //NetworkManager.Singleton.NetworkConfig.ConnectionData = data;
         NetworkManager.Singleton.StartClient();
         //Debug.Log("Numero de jogadores: " + NetworkManager.Singleton.ConnectedClients.Count);
     }
@@ -84,10 +102,26 @@ public class PasswordNetworkController : MonoBehaviour
 
     private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
     {
+        //if (NetworkManager.Singleton.IsHost)
+        //{
+        //    response.Approved = true;
+        //    response.CreatePlayerObject = true;
+        //    response.PlayerPrefabHash = null;
+        //    response.Position = new Vector3(0f, 1f, -6f);
+        //    response.Rotation = Quaternion.Euler(0f, 180f, 0f);
+        //    response.Pending = false;
+        //    return;
+        //}
+
         var clientId = request.ClientNetworkId;
         var connectionData = request.Payload;
-
+        //byte[] connectionData = request.Payload;
+        //Player payload = new Player();
+        //payload = JsonUtility.FromJson<Player>(Encoding.ASCII.GetString(connectionData));
+        //Debug.Log(roomData[NetworkManager.Singleton.LocalClientId]);
         bool approveConnection = Encoding.ASCII.GetString(connectionData) == passwordInputField.text;
+        //bool approveConnection = payload.password == passwordInputField.text;
+        Debug.Log(Encoding.ASCII.GetString(connectionData));
 
         Vector3 spawnPos = Vector3.zero;
         Quaternion spawnRot = Quaternion.identity;

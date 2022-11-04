58312f6d05391f678c16f007792582a9d2d2070b
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-11-04 00:07:41 -0300

    Created first version of position randomizer

---
+++ Assets/PositionRandomizer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PositionRandomizer : NetworkBehaviour
+{
+    private void Awake()
+    {
+        NetworkObject player = NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject;
+        float x = Random.Range(-30, 30);
+        float y = Random.Range(-30, 30);
+        player.transform.position = new Vector3(x, 0, y);
+    }
+}

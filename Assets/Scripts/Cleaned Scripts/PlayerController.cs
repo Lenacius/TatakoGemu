2f69b39b1f06a64d4d326316046dd76dd7fdeffe
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-11-14 21:50:33 -0300

    Created logic to deal with other cameras

--- Assets/Scripts/Cleaned Scripts/PlayerController.cs
+++ Assets/Scripts/Cleaned Scripts/PlayerController.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 public class PlayerController : CharacterController
 {
+    public override void OnNetworkSpawn()
+    {
+        //base.OnNetworkSpawn();
+        DeleteNonPlayerCameras();
+    }
+
     void Update()
     {
         if (IsOwner)
@@ -23,5 +29,21 @@ public class PlayerController : CharacterController
 
         if (Input.GetAxis("Mouse X") > 0) Rotate(Direction.RIGHT);
         else if (Input.GetAxis("Mouse X") < 0) Rotate(Direction.LEFT);
+    }
+
+    [SerializeField] private Camera player_camera;
+    private void DeleteNonPlayerCameras()
+    {
+        GetPlayerCamera();
+        foreach(Camera camera in Camera.allCameras)
+        {
+            if (camera != player_camera)
+                Destroy(camera.gameObject);
+        }
+    }
+
+    private void GetPlayerCamera()
+    {
+        player_camera = GetComponentInChildren<Camera>();
     }
 }

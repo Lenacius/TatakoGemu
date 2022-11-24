0181fbd7688300ca1b5cb59f4865c8abdf8153ea
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-11-24 19:32:33 -0300

    Fixed randomizer so it doesn't spawn right at the portal

--- Assets/PositionRandomizer.cs
+++ Assets/PositionRandomizer.cs
@@ -8,8 +8,20 @@ public class PositionRandomizer : NetworkBehaviour
     private void Awake()
     {
         NetworkObject player = NetworkManager.Singleton.LocalClient.PlayerObject;
-        float x = Random.Range(-30, 30);
-        float y = Random.Range(-30, 30);
+        float x = 0;
+        float y = 0;
+        while(ValueInBound(x) && ValueInBound(y))
+        {
+            x = Random.Range(-30, 30);
+            y = Random.Range(-30, 30);
+        }
         player.transform.position = new Vector3(x, 0, y);
+    }
+
+    private bool ValueInBound(float x)
+    {
+        if (x <= 5 && x >= -5)
+            return true;
+        return false;
     }
 }
